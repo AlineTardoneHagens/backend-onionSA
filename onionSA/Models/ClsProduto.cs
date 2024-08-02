7b4dd580ad8937d87a4ea86ad1eb183727712bf5
AlineTardoneHagens <aline.tardone@hagens.com.br>
2024-08-02 11:02:11 -0300

    Excluido projeto monolitico

--- onionSA/Models/ClsProduto.cs
+++
@@ -1,9 +0,0 @@
-namespace OnionSA.Models
-{
-    public class ClsProduto
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public decimal Valor { get; set; }
-    }
-}
